5902f8080d59abd255c2284855304ec9c6eccf47
DanLoad17 <105721393+DanLoad17@users.noreply.github.com>
2024-12-19 19:58:33 -0500

    Add files via upload

---
+++ Lab1/ALUnBarrelshift/ALU.cpp
@@ -0,0 +1,12 @@
+// ALU CODE
+//////////////////////////////////
+#include "ALU.h"
+
+void ALU :: ALU_method(){
+	if(OP.read() == 1){ //addition
+		alu_out.write(A.read()+B.read());		
+	}
+	else{ //subtraction
+		alu_out.write(A.read()-B.read());
+	}
+}
